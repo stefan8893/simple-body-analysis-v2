dc52c122a60dd078bb525f9d1ed229de0e0a22e2
Stefan <stefan.jeitler@outlook.com>
2024-09-03 12:02:18 +0200

    feat: date range picker enhancements

--- src/app/components/miscellaneous/date-range-picker/date-range-picker.component.ts
+++ src/app/components/miscellaneous/date-range-picker/date-range-picker.component.ts
@@ -1,36 +1,151 @@
 import {
   Component,
+  computed,
+  effect,
   input,
   model,
   OnInit,
+  output,
   ViewEncapsulation,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
   addDays,
   endOfDay,
+  endOfYear,
+  format,
   parseISO,
   startOfDay,
   startOfYear,
   subDays,
   subMonths,
+  subYears,
 } from 'date-fns';
+import { toZonedTime } from 'date-fns-tz';
 import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
 import { MenuModule } from 'primeng/menu';
+
+type QuickSelection = {
+  name: string;
+  code: string;
+  range: () => Date[];
+};
 
 @Component({
   selector: 'app-date-range-picker',
   standalone: true,
-  imports: [CalendarModule, MenuModule, FormsModule],
+  imports: [CalendarModule, MenuModule, DropdownModule, FormsModule],
   templateUrl: './date-range-picker.component.html',
   styleUrl: './date-range-picker.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class DateRangePickerComponent implements OnInit {
-  dateRange = model<Date[] | undefined>(undefined);
+  dateRangeRaw = model<Date[] | undefined>(undefined);
+  preparedDateRangeChanged = output<string[]>();
   initialRange = input<Date[] | null>(null);
-  minDate = parseISO('2021-01-01T00:00:00Z');
+  minDate = parseISO('2000-01-01T00:00:00Z');
   maxDate = addDays(new Date(), 2);
+
+  quickSelections: QuickSelection[] = [
+    {
+      code: 'L7D',
+      name: 'Letzte 7 Tage',
+      range: () => {
+        const from = startOfDay(subDays(new Date(), 6));
+        const to = endOfDay(new Date());
+
+        return [from, to];
+      },
+    },
+    {
+      code: 'L30D',
+      name: 'Letzte 30 Tage',
+      range: () => {
+        const from = startOfDay(subDays(new Date(), 29));
+        const to = endOfDay(new Date());
+
+        return [from, to];
+      },
+    },
+    {
+      code: 'L2M',
+      name: 'Letzte 2 Monate',
+      range: () => {
+        const from = startOfDay(subMonths(addDays(new Date(), 1), 2));
+        const to = endOfDay(new Date());
+
+        return [from, to];
+      },
+    },
+    {
+      code: 'L6M',
+      name: 'Letzte 6 Monate',
+      range: () => {
+        const from = startOfDay(subMonths(addDays(new Date(), 1), 6));
+        const to = endOfDay(new Date());
+
+        return [from, to];
+      },
+    },
+    {
+      code: 'CY',
+      name: 'Akutelles Jahr',
+      range: () => {
+        const from = startOfYear(new Date());
+        const to = endOfDay(new Date());
+
+        return [from, to];
+      },
+    },
+    {
+      code: 'LY',
+      name: 'Letztes Jahr',
+      range: () => {
+        const oneYearAgo = subYears(new Date(), 1);
+        const from = startOfYear(oneYearAgo);
+        const to = endOfYear(oneYearAgo);
+
+        return [from, to];
+      },
+    },
+    {
+      code: 'L2Y',
+      name: 'Letzte 2 Jahre',
+      range: () => {
+        const from = subYears(addDays(new Date(), 1), 2);
+        const to = endOfDay(new Date());
+
+        return [from, to];
+      },
+    },
+  ];
+
+  selectedQuickSelection: QuickSelection | undefined;
+  showQuickSelectionPlaceholder = computed(
+    () => this.dateRangeRaw()?.length === 0
+  );
+
+  constructor() {
+    effect(() => {
+      const [from, to] = this.dateRangeRaw() ?? [];
+
+      if (!from || !to) return;
+
+      const timeZone = 'Europe/Vienna';
+      const fromLocalTime = startOfDay(toZonedTime(from, timeZone));
+      const toLocalTime = endOfDay(toZonedTime(to, timeZone));
+
+      const fromSearchString = this.formatToSearchString(fromLocalTime);
+      const toSearchString = this.formatToSearchString(toLocalTime);
+
+      this.preparedDateRangeChanged.emit([fromSearchString, toSearchString]);
+    });
+  }
+
+  private formatToSearchString(date: Date) {
+    return format(date, `yyyy-MM-dd'T'HH:mm`);
+  }
 
   ngOnInit(): void {
     const [initialFrom, initialTo] = this.initialRange() ?? [];
@@ -38,60 +153,17 @@ export class DateRangePickerComponent implements OnInit {
     if (!initialFrom || !initialTo) return;
 
     if (initialFrom >= this.minDate && initialTo <= this.maxDate) {
-      this.dateRange.set([initialFrom, initialTo]);
+      this.dateRangeRaw.set([initialFrom, initialTo]);
     }
   }
 
-  fastSelectionItems = [
-    {
-      label: 'Schnellauswahl',
-      items: [
-        {
-          label: 'Letzte 7 Tage',
-          command: () => {
-            const from = startOfDay(subDays(new Date(), 6));
-            const to = endOfDay(new Date());
+  onDateRangeSelected(event: any) {
+    this.selectedQuickSelection = undefined;
+  }
 
-            this.dateRange.update(() => [from, to]);
-          },
-        },
-        {
-          label: 'Letzte 28 Tage',
-          command: () => {
-            const from = startOfDay(subDays(new Date(), 27));
-            const to = endOfDay(new Date());
+  onQuickSelectionChanged({ value }: { value: QuickSelection | undefined }) {
+    if (!value) return;
 
-            this.dateRange.update(() => [from, to]);
-          },
-        },
-        {
-          label: 'Letzte 2 Monate',
-          command: () => {
-            const from = startOfDay(subMonths(subDays(new Date(), 1), 2));
-            const to = endOfDay(new Date());
-
-            this.dateRange.update(() => [from, to]);
-          },
-        },
-        {
-          label: 'Letzte 6 Monate',
-          command: () => {
-            const from = startOfDay(subMonths(subDays(new Date(), 1), 6));
-            const to = endOfDay(new Date());
-
-            this.dateRange.update(() => [from, to]);
-          },
-        },
-        {
-          label: 'Akutelles Jahr',
-          command: () => {
-            const from = startOfYear(new Date());
-            const to = endOfDay(new Date());
-
-            this.dateRange.update(() => [from, to]);
-          },
-        },
-      ],
-    },
-  ];
+    this.dateRangeRaw.update(() => value.range());
+  }
 }
--- src/app/components/table-view/table-view.component.ts
+++ src/app/components/table-view/table-view.component.ts
@@ -1,20 +1,9 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  computed,
-  effect,
-  signal,
-  ViewEncapsulation,
-} from '@angular/core';
+import { Component, signal, ViewEncapsulation } from '@angular/core';
 import { TableClient, TableEntityResult } from '@azure/data-tables';
-import { endOfDay, format } from 'date-fns';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { BodyAnalysis } from '../../body-analysis-data/body-analysis.types';
-import {
-  AzureTablesFilter,
-  BodyAnalysisFilter,
-} from '../../body-analysis-data/common.types';
 import { FormatDatePipe } from '../../pipes/date-fns-format.pipe';
 import { ContentHeaderComponent } from '../miscellaneous/content-header/content-header.component';
 import { DateRangePickerComponent } from '../miscellaneous/date-range-picker/date-range-picker.component';
@@ -35,46 +24,11 @@ import { DateRangePickerComponent } from '../miscellaneous/date-range-picker/dat
   encapsulation: ViewEncapsulation.None,
 })
 export class TableViewComponent {
-  dateRangeFilter = signal<Date[]>([]);
+  dateRangeRaw = signal<Date[]>([]);
   rows: BodyAnalysis[] = [];
   isLoading = false;
-  isRefreshButtonEnabled = computed(
-    () => this.dateRangeFilter()?.filter((x) => !!x).length === 2
-  );
 
-  constructor(private bodyAnalysisTable: TableClient) {
-    effect(() => {
-      const changed = this.dateRangeFilter();
-      this.loadTableData();
-    });
-  }
-
-  private convertToLocal(utc: Date) {
-    return new Date(utc.getTime() + utc.getTimezoneOffset() + 1000);
-  }
-
-  private formatToSearchString(date: Date) {
-    return format(date, `yyyy-MM-dd'T'HH:mm`);
-  }
-
-  private prepareAzureTablesFilter(): BodyAnalysisFilter {
-    const [from, to] = this.dateRangeFilter() ?? [];
-    if (!from || !to) {
-      return { type: 'invalid', reason: `Missing 'from' or 'to' date.` };
-    }
-
-    const fromLocalTime = this.convertToLocal(from);
-    const toLocalTime = endOfDay(this.convertToLocal(to));
-
-    const fromSearchString = this.formatToSearchString(fromLocalTime);
-    const toSearchString = this.formatToSearchString(toLocalTime);
-
-    return {
-      type: 'filter',
-      from: fromSearchString,
-      to: toSearchString,
-    };
-  }
+  constructor(private bodyAnalysisTable: TableClient) {}
 
   private mapEntityToBodyAnalysis(
     entity: TableEntityResult<Record<string, unknown>>
@@ -90,25 +44,25 @@ export class TableViewComponent {
     };
   }
 
-  private queryAzureTables(filter: AzureTablesFilter) {
+  private queryAzureTables(from: string, to: string) {
     return this.bodyAnalysisTable.listEntities({
       queryOptions: {
-        filter: `PartitionKey eq 'body_data' and RowKey ge '${filter.from}' and RowKey le '${filter.to}'`,
+        filter: `PartitionKey eq 'body_data' and RowKey ge '${from}' and RowKey le '${to}'`,
       },
     });
   }
 
-  async loadTableData() {
-    const filter = this.prepareAzureTablesFilter();
-    if (filter.type === 'invalid') {
-      console.debug('Invalid Filter:', filter.reason);
-      return;
-    }
+  onPreparedDateRangeChanged(event: string[]) {
+    const [from, to] = event;
+    console.log(event);
+    this.loadTableData(from, to);
+  }
 
+  async loadTableData(from: string, to: string) {
     try {
       this.isLoading = true;
 
-      let entitiesIterator = this.queryAzureTables(filter);
+      let entitiesIterator = this.queryAzureTables(from, to);
 
       let result: BodyAnalysis[] = [];
       for await (const entity of entitiesIterator) {
@@ -122,9 +76,5 @@ export class TableViewComponent {
     } finally {
       this.isLoading = false;
     }
-  }
-
-  async refresh() {
-    this.loadTableData();
   }
 }
